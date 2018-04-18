b4347ba3520b5557dbb8c058b821a47b04c7297d
Andrius Viederis <andrius.viederis@gmail.com>
2018-04-18 22:23:24 +0200

    Added Production.json setting files; Updated gitignore file;

--- AuthorizationServer/Startup.cs
+++ AuthorizationServer/Startup.cs
@@ -32,9 +32,9 @@ namespace AuthorizationServer
 
             var builder = new ConfigurationBuilder()
              .SetBasePath(env.ContentRootPath)
-             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-             .AddJsonFile("appsettings-custom.json", optional: true)
+             .AddJsonFile("appsettings-custom.json", optional: false)
              .AddJsonFile($"appsettings-custom.{env.EnvironmentName}.json", optional: true)
              .AddEnvironmentVariables();
             Configuration = builder.Build();
